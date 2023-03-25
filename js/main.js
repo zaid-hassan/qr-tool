c979eb09e08f0dee1b1b9e8ea2363c22ad677e5a
zaid-hassan <zaidhassan251@gmail.com>
2023-03-25 15:09:34 +0530

    Completed the Qr code generation part, with a color scheme for the page.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-var qrgen = new QRCode("qrgen", "https://www.geeksforgeeks.org");
+let qrcodeform = document.getElementById("qrcode-generator")
+let qrcodeinput = document.getElementById("qrcode-input")
+let qrCode;
+
+function generateQRcode(qrinput) {
+    return new QRCode("qrgen", {
+        text: qrinput,
+        width: 228,
+        height: 228,
+        colorDark: "#0F0E11",
+        colorLight: "#2B2D35",
+        correctLevel : QRCode.CorrectLevel.H
+    });
+}
+
+// Event Listner for form submition 
+qrcodeform.addEventListener("submit", function(event){
+    // Prevent form submission
+    event.preventDefault();
+    let qrinput = qrcodeinput.value;
+    if(qrCode == null) {
+        // Generate code initially
+        qrCode = generateQRcode(qrinput);
+    }
+    else {
+        // If code already generated
+        qrCode.makeCode(qrinput);
+    }
+})
